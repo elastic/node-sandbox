4c5f0b6ab507c5210aa4de2b87f7a0c89b051bec
Jordan Sissel <jls@semicomplete.com>
2018-01-13 23:49:31 -0800

    Conditional compilation for execveat(2) which was only added in Linux 3.19 and is not available on Travis-ci

--- native/sandbox_linux.cpp
+++ native/sandbox_linux.cpp
@@ -9,6 +9,13 @@
 
 #include <string.h> /* for strerror */
 
+// execveat was added in Linux 3.19
+#ifndef __NR_execveat
+#define JMP_START 4
+#else
+#define JMP_START 5
+#endif
+
 struct sock_filter reject_syscalls[] = {
   // TODO(sissel): Verify arch of syscall
   //BPF_STMT(BPF_LD|BPF_W|BPF_ABS, (offsetof(struct seccomp_data, arch))),
@@ -20,11 +27,14 @@ struct sock_filter reject_syscalls[] = {
 
   // JMP|JEQ|K Do a jump after comparing EQuality of the loaded value and a
   // constant. If equal, jump 2 positions forward, if not equal, do not jump(zero jump).
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_seccomp, 5, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_fork, 4, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_vfork, 3, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execve, 2, 0),
-  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execveat, 1, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_seccomp, JMP_START, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_fork, JMP_START - 1, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_vfork, JMP_START - 2, 0),
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execve, JMP_START - 3, 0),
+
+#ifdef __NR_execveat
+  BPF_JUMP(BPF_JMP|BPF_JEQ|BPF_K, __NR_execveat, JMP_START - 4, 0),
+#endif
 
   // RET|K Return a constant.
   // If none of the conditions above are met, let's allow this syscall. 
