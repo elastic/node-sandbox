bde959cede517f43121e34c58de72fb3ac484b00
Jordan Sissel <jls@semicomplete.com>
2018-01-25 16:38:08 -0800

    Windows throws 'spawn UNKNOWN'

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ const sandbox = require(`./build/Release/sandbox`);
 const child_process = require("child_process");
 
 function tryExec() {
+  // child_process.spawn() throws an exception immediately if it fails.
+  // so there's no need to bind to the 'error' event.
   child_process.exec("true");
 }
 
@@ -12,5 +14,5 @@ assert.doesNotThrow(tryExec);
 let s = sandbox.activate()
 assert.ok(s.success, "Sandbox failed to activate: " + s.message);
 
-// child_process.spawn() throws an exception immediately if it fails.
-assert.throws(tryExec, /Error: spawn EACCES/);
+// EACCES on Linux, UNKNOWN on Windows.
+assert.throws(tryExec, /Error: spawn (EACCES|UNKNOWN)/);
